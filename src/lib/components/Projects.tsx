669d3a166efb5bbcf0da99bb43123b0482f2e5fa
Chigozie Ezeh <chigozie@constructiontechinstitute.org>
2025-03-01 09:08:44 +0100

    close menu on mobile after selection

--- src/lib/components/Projects.tsx
+++ src/lib/components/Projects.tsx
@@ -56,6 +56,10 @@ export const Projects: React.FC<{
               <button
                 key={`project-${idx}`}
                 className="opacity-90 border-2 bg-[#1e1e1e] border-[#323232] w-full mb-2 hover:opacity-100 transition-opacity duration-300 rounded-md p-2"
+                onClick={() => {
+                  setSelectedProject(project);
+                  setMenuOpen(false);
+                }}
               >
                 {project}
               </button>
