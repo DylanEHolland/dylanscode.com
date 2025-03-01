d1e3b00ec52289589855e19b17be65686490470e
Chigozie Ezeh <chigozie@constructiontechinstitute.org>
2025-03-01 09:51:04 +0100

    removed w-full from button to capture the size of the icon

--- src/lib/components/Projects.tsx
+++ src/lib/components/Projects.tsx
@@ -53,7 +53,7 @@ export const Projects: React.FC<{
       >
         <div className="sm:hidden mb-4">
           <button
-            className="bg-black/60 border-2 border-black/10 text-white p-2 rounded-md w-full flex items-center justify-center"
+            className="bg-black/60 border-2 border-black/10 text-white p-2 rounded-md flex items-center justify-center"
             onClick={() => setMenuOpen(!menuOpen)}
           >
             {menuOpen ? (
